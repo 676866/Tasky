fdb1409bb599be7dd12cdb56da9d0f5b4c6e02df
Your Name <your.email@example.com>
2025-07-27 19:48:41 +0300

    remove the localhost url

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://tasky-umber-two.vercel.app"],
+    origin: [ "https://tasky-umber-two.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
