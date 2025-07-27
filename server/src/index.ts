67178095a33b24136190033dff4cbd43898126d5
Your Name <your.email@example.com>
2025-07-27 20:18:39 +0300

    updated CORS to allow vercel frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,27 +8,16 @@ import { authenticate } from "./middleware/authMiddleware";
 dotenv.config();
 
 const app = express();
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://tasky-umber-two.vercel.app"
-];
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      
+      "https://tasky-umber-two.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
-
-app.options("*", cors());
 
 app.use(express.json());
 
